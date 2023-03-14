531bf06749c2eb5b0b908509f4a1d4e8d754954f
senpainikolay <nicolae32gherman@gmail.com>
2023-03-14 22:55:58 +0200

    add bonus week2

--- lib/Load_Balancer.ex
+++ lib/Load_Balancer.ex
@@ -11,18 +11,30 @@ defmodule LoadBalancer do
 
 
   def handle_info(chunkData, state) do
-    pr = :"Printer#{state}"
-    state = state + 1
-    if Process.whereis(pr) == nil do
-      send(__MODULE__,chunkData)
-      {:noreply, state}
-    else
-    cond do
-      state >= 4 -> send(pr, chunkData);      {:noreply, 1}
-      true -> send(pr, chunkData);      {:noreply, state}
-    end
+    Enum.reduce(1..3, %{}, fn pidNum, acc -> Map.put(acc, :"Printer#{pidNum}", Process.info(Process.whereis(:"Printer#{pidNum}"), :message_queue_len ) ) end)
+    |> Map.to_list()
+    |> Enum.min_by(fn {_, minMessageQueueLen} -> minMessageQueueLen end)
+    |> elem(0)
+    |> Process.whereis
+    |> send(chunkData)
+    {:noreply, state}
   end
-  end
+
+
+  #  Round Robin fashion.
+  # def handle_info(chunkData, state) do
+  #   pr = :"Printer#{state}"
+  #   state = state + 1
+  #   if Process.whereis(pr) == nil do
+  #     send(__MODULE__,chunkData)
+  #     {:noreply, state}
+  #   else
+  #   cond do
+  #     state >= 4 -> send(pr, chunkData);      {:noreply, 1}
+  #     true -> send(pr, chunkData);      {:noreply, state}
+  #   end
+  # end
+  # end
 
   def handle_call(:killMessage,_from, state) do
     pr = :"Printer#{state}"
